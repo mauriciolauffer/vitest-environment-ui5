f2776625ab3045c8fd0cbf062f0a49cb5ffea410
Mauricio Lauffer <443888+mauriciolauffer@users.noreply.github.com>
2025-07-11 22:46:41 +0200

    feat: Support UI5 legacy free

--- test/env-ui5/file.test.ts
+++ test/env-ui5/file.test.ts
@@ -3,10 +3,13 @@ import { expect, describe, it } from "vitest";
 describe("Load test from file", () => {
   it("ui5 env is defined", () => {
     const environmentOptions =
-      globalThis?.__vitest_worker__?.ctx?.config?.environmentOptions;
+      globalThis?.__vitest_worker__?.config?.environmentOptions;
+    const environmentOptionsDocBlock =
+      globalThis?.__vitest_worker__?.ctx?.environment?.options;
     const ui5Options = { ui5: { path: "../fixtures/ui5.html", timeout: 1000 } };
     expect(expect.getState().environment).toBe("ui5");
     expect(environmentOptions).toMatchObject(ui5Options);
+    expect(environmentOptionsDocBlock).toBeFalsy();
   });
 
   it("jsdom is initiated", () => {
